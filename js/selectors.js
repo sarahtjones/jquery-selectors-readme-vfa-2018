4dc68967f76d8f1aece7cad01e53d0f59a866908
Sarah Jones <saraht.jones8@gmail.com>
2018-06-29 18:11:29 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,9 +4,17 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
-  $('img:last')
+  return $('img:last')
 }
 
 function ninjaBabySelector() {
-  return $('p');
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('ul li:first-child');
 }
